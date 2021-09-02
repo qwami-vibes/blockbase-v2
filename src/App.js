41d9d9564ae16cfe483927197dac136e9bf69bf5
Kwaame Ofori-Adjekum <44780028+qwami-vibes@users.noreply.github.com>
2021-09-02 03:41:51 +0000

    Added api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 
-// import Signup from "./auth/Signup";
+import Signup from "./auth/Signup";
 import Dashboard from "./pages/Dashboard";
 import GlobalStyles from "./components/GlobalStyles";
 
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <Switch>
-        {/* <Route exact path="/" component={Signup} /> */}
-        <Route path="/" component={Dashboard} />
+        <Route exact path="/" component={Signup} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
   );
