3f7d684e0fbc05493d6beaad5f023a3d054019c4
Kwaame Ofori-Adjekum <44780028+qwami-vibes@users.noreply.github.com>
2021-09-02 03:39:33 +0000

    temp testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 
-import Signup from "./auth/Signup";
+// import Signup from "./auth/Signup";
 import Dashboard from "./pages/Dashboard";
 import GlobalStyles from "./components/GlobalStyles";
 
